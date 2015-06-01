c133befceeb1f2a4327db39177b26055429b739b
gwulfs <you@example.com>
2015-06-01 16:51:20 -0700

    dtypes

--- mkdb.py
+++ mkdb.py
@@ -1,7 +1,9 @@
+import sqlalchemy
 import pandas as pd
+
 df = pd.read_csv('data/PFALL15_new.csv')
+engine = sqlalchemy.create_engine('sqlite:///hb.db')
 
-from sqlalchemy import create_engine
-engine = create_engine('sqlite:///hb.db')
+types = {'carrier': sqlalchemy.types.Integer, 'hcpcs': sqlalchemy.types.String, 'locality': sqlalchemy.types.Integer, 'facFee': sqlalchemy.types.Float, 'nonFacFee': sqlalchemy.types.Float}
 
-df.to_sql("hb2", engine, if_exists='replace')
+df.to_sql("hb2", engine, if_exists='replace', dtype=types)
